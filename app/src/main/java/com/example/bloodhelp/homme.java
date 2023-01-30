a9428f0b2b441720cbfffabb6c4e033c0e85522a
amine1949 <amine.barik.2001@gmail.com>
2023-01-30 20:54:38 +0100

    Navigation Bar

--- app/src/main/java/com/example/bloodhelp/homme.java
+++ app/src/main/java/com/example/bloodhelp/homme.java
@@ -1,14 +1,42 @@
 package com.example.bloodhelp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
 public class homme extends AppCompatActivity {
 
+    ChipNavigationBar chipNavigationBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homme);
+
+        chipNavigationBar = findViewById(R.id.bottom_nav_bar);
+        chipNavigationBar.setItemSelected(R.id.Home,true);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new homeFragment()).commit();
+
+        chipNavigationBar.setOnItemSelectedListener(new ChipNavigationBar.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(int i) {
+                Fragment fragment = null;
+                switch (i){
+                    case R.id.Home:
+                        fragment = new homeFragment();
+                        break;
+                    case R.id.Historique:
+                        fragment = new historyFragment();
+                        break;
+                    case R.id.Profile:
+                        fragment = new profileFragment();
+                        break;
+                }
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,fragment).commit();
+            }
+        });
     }
 }
